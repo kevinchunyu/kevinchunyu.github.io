69773b8740778968fffcac884e19f9d5559f0329
kevinchunyu <>
2021-06-13 12:50:09 +0800

    styling updates to website

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-/*!
-* Start Bootstrap - Resume v7.0.1 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
 
 window.addEventListener('DOMContentLoaded', event => {
 
