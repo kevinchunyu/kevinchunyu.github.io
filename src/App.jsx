6d1f7357ea89ae97946536a9ba7bfe983ca4ce40
kko <i-kko@terrapower.com>
2022-09-06 10:44:49 -0700

    added tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import {React, Component} from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Contact from './components/contacts/Contact'
+import ReactGA from 'react-ga';
 
-const App = () => {
-  return (
-    <>
-      <Header />
-      <Nav />
-      <About />
-      <Experience />
-      <Contact />
-    </>
-  )
+class App extends Component {
+  setGA = () =>  {
+    ReactGA.initialize('G-R4HYR6HY9X');
+    ReactGA.pageview(window.location.pathname);
+  };
+  componentDidMount() {
+    this.setGA();
+  }
+  render() {
+    return (
+      <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Contact />
+      </>
+    );
+  }
 }
 
 export default App
