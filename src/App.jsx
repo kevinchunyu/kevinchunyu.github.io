a6c7af2902512fb56dc5edfe491604fbff6993e3
Kevin Ko <kevinkcy511@gmail.com>
2023-05-15 16:44:20 -0700

    ga test2

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import ReactGA from 'react-ga';
 import React, {useEffect} from 'react'
 
-const TRACKING_ID = 'G-R4HYR6HY9X';
+const TRACKING_ID = 'G-3GM2TKFWG0';
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Routes, Route, useLocation} from "react-router-dom"
 import About from './about/About'
 import Projects from './projects/Projects'
 import Contact from './contact/contact'
 import Header from './header/Header'
 import { AnimatePresence } from 'framer-motion'
+import ReactGA from 'react-ga';
 
+const TRACKING_ID = 'G-3GM2TKFWG0';
+ReactGA.initialize(TRACKING_ID);
 
 function AnimatedRoutes() {
     const location = useLocation();
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+      }, []);
     return (
     <AnimatePresence>
         <Routes location={location} key={location.pathname}>
