3436ea3be7b0cf9b78f223d1f5ff892bd2abcb31
Kevin Ko <kevinkcy511@gmail.com>
2023-05-15 16:33:04 -0700

    test google analytics tracking

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import NavBar from './components/Navbar/Navbar'
 import AnimatedRoutes from './components/AnimatedRoutes'
 import { BrowserRouter} from "react-router-dom";
 import './index.css'
+import ReactGA from 'react-ga';
+import React, {useEffect} from 'react'
 
-
-import React from 'react'
+const TRACKING_ID = 'G-R4HYR6HY9X';
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <BrowserRouter>
       <NavBar/>
